8b224a7c0193c1cceb576c5c6890a408b0f3e3e1
Lars Lehtonen <lars.lehtonen@gmail.com>
2025-07-03 16:24:47 -0700

    summitslist support

--- go.mod
+++ go.mod
@@ -3,13 +3,6 @@ module github.com/alrs/sota
 go 1.19
 
 require (
+	github.com/alrs/phonetic v0.0.0-20230214213106-5291957b282b
 	github.com/davecgh/go-spew v1.1.1
-	github.com/fluffle/goirc v1.3.0
-)
-
-require (
-	github.com/alrs/phonetic v0.0.0-20230214213106-5291957b282b // indirect
-	github.com/emersion/go-sasl v0.0.0-20220912192320-0145f2c60ead // indirect
-	github.com/golang/mock v1.5.0 // indirect
-	golang.org/x/net v0.6.0 // indirect
 )
---
+++ summit.go
@@ -0,0 +1,110 @@
+package sota
+
+import (
+	"encoding/csv"
+	"io"
+	"os"
+	"strconv"
+)
+
+type Summit struct {
+	// SummitCode,AssociationName,RegionName,SummitName,AltM,AltFt,GridRef1,GridRef2,Longitude,Latitude,Points,BonusPoints,ValidFrom,ValidTo,ActivationCount,ActivationDate,ActivationCall
+	SummitCode      string
+	AssociationName string
+	RegionName      string
+	SummitName      string
+	AltM            int
+	AltFt           int
+	GridRef1        string
+	GridRef2        string
+	Longitude       float64
+	Latitude        float64
+	Points          int
+	BonusPoints     int
+	ValidFrom       string
+	ValidTo         string
+	ActivationCount int
+	ActivationDate  string
+	ActivationCall  string
+}
+
+func parseLine(l []string) (s Summit, err error) {
+	s.SummitCode = l[0]
+	s.AssociationName = l[1]
+	s.RegionName = l[2]
+	s.SummitName = l[3]
+	if l[4] != "" {
+		s.AltM, err = strconv.Atoi(l[4])
+	}
+	if err != nil {
+		return
+	}
+	if l[5] != "" {
+		s.AltFt, err = strconv.Atoi(l[5])
+	}
+	if err != nil {
+		return
+	}
+
+	s.GridRef1 = l[6]
+	s.GridRef2 = l[7]
+
+	if l[8] != "" {
+		s.Longitude, err = strconv.ParseFloat(l[8], 32)
+	}
+	if err != nil {
+		return
+	}
+	if l[9] != "" {
+		s.Latitude, err = strconv.ParseFloat(l[9], 32)
+	}
+	if err != nil {
+		return
+	}
+	if l[10] != "" {
+		s.Points, err = strconv.Atoi(l[10])
+	}
+	if l[11] != "" {
+		s.BonusPoints, err = strconv.Atoi(l[11])
+	}
+	//FIXME time.Time
+	s.ValidFrom = l[12]
+	s.ValidTo = l[13]
+
+	if l[14] != "" {
+		s.ActivationCount, err = strconv.Atoi(l[14])
+	}
+
+	//FIXME time.Time
+	s.ActivationDate = l[15]
+
+	s.ActivationCall = l[16]
+	return
+}
+
+func readCSV(f *os.File) (summits []Summit, err error) {
+	reader := csv.NewReader(f)
+	reader.FieldsPerRecord = 17
+	var l []string
+	// ditch two header lines, ignore error on first line
+	for i := 0; i < 2; i++ {
+		l, _ = reader.Read()
+	}
+	var s Summit
+	for i := 0; ; i++ {
+		l, err = reader.Read()
+		if err == io.EOF {
+			err = nil
+			break
+		}
+		if err != nil {
+			return
+		}
+		s, err = parseLine(l)
+		if err != nil {
+			return
+		}
+		summits = append(summits, s)
+	}
+	return
+}
